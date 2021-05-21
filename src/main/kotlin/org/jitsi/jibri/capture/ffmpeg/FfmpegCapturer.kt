3111d2a86ecab0a4ca3341b84a4160390a20ba8e
tmgrask <t.mackersy@psiphon.ca>
2021-05-21 09:39:10 -0400

    hardcode higher qual stream settings

--- src/main/kotlin/org/jitsi/jibri/capture/ffmpeg/FfmpegCapturer.kt
+++ src/main/kotlin/org/jitsi/jibri/capture/ffmpeg/FfmpegCapturer.kt
@@ -41,11 +41,11 @@ import java.util.logging.Logger
  * Parameters which will be passed to ffmpeg
  */
 data class FfmpegExecutorParams(
-    val resolution: String = FfmpegCapturer.resolution,
+    val resolution: String = "1920x1080",
     val framerate: Int = 30,
-    val videoEncodePreset: String = "veryfast",
+    val videoEncodePreset: String = "ultrafast",
     val queueSize: Int = 4096,
-    val streamingMaxBitrate: Int = 2976,
+    val streamingMaxBitrate: Int = 8000,
     val streamingBufSize: Int = streamingMaxBitrate * 2,
     // The range of the CRF scale is 0–51, where 0 is lossless,
     // 23 is the default, and 51 is worst quality possible. A lower value
@@ -54,7 +54,7 @@ data class FfmpegExecutorParams(
     // it should look the same or nearly the same as the input but it
     // isn't technically lossless.
     // https://trac.ffmpeg.org/wiki/Encode/H.264#crf
-    val h264ConstantRateFactor: Int = 25,
+    val h264ConstantRateFactor: Int = 18,
     val gopSize: Int = framerate * 2,
     val audioSource: String = FfmpegCapturer.audioSource,
     val audioDevice: String = FfmpegCapturer.audioDevice
